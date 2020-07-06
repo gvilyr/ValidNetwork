b7373c69d18c4881d891a3a95ed976cf7a26d5b7
gvilyr <gvilyr@post.bgu.ac.il>
2020-07-06 15:07:59 +0300

    first commit

---
+++ Aucation.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.19;
+contract Aucation{
+	address public manager;
+	address public seller;
+	uint public latestBid;
+	address public latestBidder;
+	constructor() public {
+		manager = msg.sender;
+	}
+	function auction(uint bid) public{
+		latestBid = bid * 1 ether;
+		seller = msg.sender;
+	}
+	function bid() public payable{
+		require(msg.value > latestBid);
+		if(latestBid ! = 0x0){
+			latestBidder.transfer(latestBid);
+		}
+		latestBidder = msg.sender;
+		latestBid = msg.value;
+	}
+	function finishAuction() restricted public{
+		seller.transfer(address(this).balance);
+	}
+	modifier restricted(){
+		require(msg.sender == manager);
+	}
+}	
